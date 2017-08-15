6d815446ef5a3c8bd82a209a3bb81321123ff7b0
SoengSaravit <soengsaravit@gmail.com>
2017-08-15 23:25:30 +0700

    Initial commit

--- PickerView-16-07-2017/ViewController.swift
+++ PickerView-16-07-2017/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var showDayLabel: UILabel!
+    
+//    var days = ["Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    
+    var data = [
+        ["1","2","3","4","5"],
+        ["A","B","C"],
+        ["a","b","c","d"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return data.count
     }
 
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return data[component].count
+    }
 
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return data[component][row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.showDayLabel.text = data[component][row]
+     //   self.view.endEditing(true)
+    }
+    
+    
 }
 
